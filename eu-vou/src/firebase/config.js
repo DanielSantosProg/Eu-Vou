39ec44e15eaeb38751833da51769e5d786cc1777
DanielSantosProg <40002858+DanielSantosProg@users.noreply.github.com>
2023-12-02 16:42:38 -0300

    Starting the project and creating routes

---
+++ eu-vou/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyD2ZQmba06JUnfH4ZV64dRTQQ7H4WHRiTU",
+	authDomain: "eu-vou-290f1.firebaseapp.com",
+	projectId: "eu-vou-290f1",
+	storageBucket: "eu-vou-290f1.appspot.com",
+	messagingSenderId: "756760881287",
+	appId: "1:756760881287:web:3b7374180684118e9b3022",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
