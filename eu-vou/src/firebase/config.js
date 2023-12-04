65f28752c542c227669e841da444589ea26de687
DanielSantosProg <danielsantos_lol@hotmail.com>
2023-12-04 08:25:34 -0300

    Created User Register

--- eu-vou/src/firebase/config.js
+++ eu-vou/src/firebase/config.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyD2ZQmba06JUnfH4ZV64dRTQQ7H4WHRiTU",
-	authDomain: "eu-vou-290f1.firebaseapp.com",
-	projectId: "eu-vou-290f1",
-	storageBucket: "eu-vou-290f1.appspot.com",
-	messagingSenderId: "756760881287",
-	appId: "1:756760881287:web:3b7374180684118e9b3022",
+  apiKey: "AIzaSyD2ZQmba06JUnfH4ZV64dRTQQ7H4WHRiTU",
+  authDomain: "eu-vou-290f1.firebaseapp.com",
+  projectId: "eu-vou-290f1",
+  storageBucket: "eu-vou-290f1.appspot.com",
+  messagingSenderId: "756760881287",
+  appId: "1:756760881287:web:3b7374180684118e9b3022",
 };
 
 const app = initializeApp(firebaseConfig);
--- eu-vou/src/pages/Register/Register.js
+++ eu-vou/src/pages/Register/Register.js
@@ -1,5 +1,6 @@
 import styles from "./Register.module.css";
 import { useState, useEffect } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 const Register = () => {
   const [userName, setUserName] = useState("");
@@ -8,7 +9,9 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     setError("");
@@ -24,8 +27,16 @@ const Register = () => {
       return;
     }
 
-    console.log(user);
+    const res = await createUser(user);
+
+    console.log(res);
   };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
 
   return (
     <div className={styles.register}>
@@ -70,7 +81,12 @@ const Register = () => {
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
-          <button className="btn">Cadastrar</button>
+          {!loading && <button className="btn">Cadastrar</button>}
+          {loading && (
+            <button className="btn" disabled>
+              Aguarde...
+            </button>
+          )}
           {error && <p className="error">{error}</p>}
         </label>
       </form>
